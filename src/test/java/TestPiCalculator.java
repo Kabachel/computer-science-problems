59a2697fb3f5430550c52fe45ae8a549bc70c75b
Danila Shabanov <shabanov.danila.o@gmail.com>
2022-12-17 14:28:17 +0300

    test: PiCalculator

---
+++ src/test/java/TestPiCalculator.java
@@ -0,0 +1,25 @@
+import org.example.PiCalculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPiCalculator {
+
+    boolean compare;
+    @Test
+    public void testCalculatePi() {
+        compare = Math.abs(PiCalculator.calculatePi(1) - 4.0) < 1e-6;
+        Assert.assertTrue("testCalculatePi(1)", compare);
+
+        compare = Math.abs(PiCalculator.calculatePi(2) - 2.666666) < 1e-6;
+        Assert.assertTrue("testCalculatePi(2)", compare);
+
+        compare = Math.abs(PiCalculator.calculatePi(5) - 3.339682) < 1e-6;
+        Assert.assertTrue("testCalculatePi(5)", compare);
+
+        compare = Math.abs(PiCalculator.calculatePi(100) - 3.131592) < 1e-6;
+        Assert.assertTrue("testCalculatePi(100)", compare);
+
+        compare = Math.abs(PiCalculator.calculatePi(1000000) - 3.141591) < 1e-6;
+        Assert.assertTrue("testCalculatePi(1000000)", compare);
+    }
+}
