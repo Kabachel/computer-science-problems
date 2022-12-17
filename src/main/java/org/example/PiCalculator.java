9cc689101286c3105bb2e062b8fd1bd688c789b0
Danila Shabanov <shabanov.danila.o@gmail.com>
2022-12-17 14:27:54 +0300

    feat: new class PiCalculator

---
+++ src/main/java/org/example/PiCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/**
+ * Calculating number Pi via Leibniz formula
+ */
+public class PiCalculator {
+    /**
+     * Calculate number Pi with a certain terms
+     *
+     * @param nTerms number of terms
+     * @return number Pi
+     */
+    public static double calculatePi(int nTerms) {
+        final double numeration = 4.0;
+        double denomination = 1.0;
+        double operation = 1.0;
+        double pi = 0.0;
+
+        for (int i = 0; i < nTerms; i++) {
+            pi += operation * (numeration / denomination);
+            denomination += 2.0;
+            operation *= -1.0;
+        }
+
+        return pi;
+    }
+}
